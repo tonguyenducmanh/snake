05e3c1bd2ff82176e623b94fd406edbf64d83238
to nguyen duc manh <ducmanh14032000@gmail.com>
2023-05-06 10:29:13 +0700

    init gamesize

---
+++ src/components/MBoard.vue
@@ -0,0 +1,123 @@
+/**
+    màn hình thông tin của trò chơi
+    created by tdmanh1 06/05/2023
+ */
+<template>
+    <div class="m-board">
+        <div class="m-title">
+            Scores
+        </div>
+        <div class="m-score">
+            {{score}}
+        </div>
+        <div class="m-size">
+            <div class="m-size-tile">
+                Game-size: {{gameSize}} x  {{gameSize}}
+            </div>
+            <div class="m-size-button" @click="gameSizeChange(true)">Up</div>
+            <div class="m-size-button" @click="gameSizeChange(false)">Down</div>
+        </div>
+        <div class="m-tutorial">
+            <div class="m-tutorial-title">
+                Tutorial
+            </div>
+            <div class="m-tutorial-content">
+                <div class="m-tutorial-item"></div>
+                <div class="m-tutorial-item">w</div>
+                <div class="m-tutorial-item"></div>
+                <div class="m-tutorial-item">a</div>
+                <div class="m-tutorial-item">s</div>
+                <div class="m-tutorial-item">d</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            score: 0, // điểm hiện tại của trò chơi
+            gameSize: 7, // kích cỡ của trò chơi hiện tại
+            maxGameSize: 15, // kích cỡ lớn nhất của game
+            minGameSize: 5, // kích cỡ nhỏ nhất của game
+        }
+    },
+    methods:{
+        /**
+         * method tăng, giảm size của game lên 1 đơn vị
+         */
+        gameSizeChange(isIncrease){
+            let me = this
+            // thay đổi giá trị hiển thị trên thông tin
+            if(!isIncrease){
+                if(me.gameSize > me.minGameSize){
+                    me.gameSize --
+                }
+            }else{
+                if(me.gameSize < me.maxGameSize){
+                    me.gameSize ++
+                }
+            }
+            // thay đổi giá trị của các ô trên màn hình game
+            me.$emit("changeGameSize", isIncrease)
+        },
+    }
+}
+</script>
+<style scoped>
+    .m-board{
+        background-color: black;
+        height: 100%;
+        width: 40%;
+        border-radius: var(--border-radius);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        row-gap:var(--padding-common);
+        color: white;
+    }
+    .m-title{
+        /* background-color: azure; */
+        padding: 10px;
+        font-size: 1.5rem;
+        font-weight: 600;
+    }
+    .m-score{
+        font-size: 3rem;
+    }
+    .m-tutorial{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        row-gap: var(--padding-common)
+    }
+    .m-tutorial-title{
+        /* background-color: chartreuse; */
+    }
+    .m-tutorial-content{
+        /* background-color: crimson; */
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        height: 100px;
+        width: 250px;
+    }
+    .m-tutorial-item{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 1.5rem;
+        text-transform: uppercase;
+        border: 1px solid white;
+    }
+    .m-size{
+        display: flex;
+        column-gap: var(--padding-common);
+    }
+    .m-size-button{
+        cursor: pointer;
+    }
+    .m-size-button:hover{
+        background-color: rgba(139, 139, 139, 0.681);
+    }
+</style>
---
+++ src/components/MGame.vue
@@ -0,0 +1,57 @@
+/**
+    màn hình trò chơi
+    created by tdmanh1 06/05/2023
+ */
+<template>
+    <div class="m-game">
+        <div class="m-fence" :style="gameSizeStyle">
+            <!-- tạo ra bảng dựa vào kích thước của gameSize -->
+            <template v-for="index in gameSize" :key="index">
+                <template v-for="index in gameSize" :key="index">
+                    <div class="m-cube" ></div>
+                </template>
+            </template>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        // kích cỡ của trò chơi, mặc định là ô 7x7
+        gameSize:{
+            type: Number,
+            default: 7,
+        }
+    },
+    computed:{
+        gameSizeStyle(){
+            return {
+                'grid-template-columns': `repeat(${this.gameSize}, 1fr)`
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+    .m-game{
+        background-color: black;
+        height: 100%;
+        width: 60%;
+        border-radius: var(--border-radius);
+        padding: var(--padding-common);
+    }
+    .m-fence{
+        width: 100%;
+        height: 100%;
+        box-sizing: border-box;
+        border: 1px solid white;
+        /* background-color: cadetblue; */
+        display: grid;
+        grid-template-columns: repeat(gameSize, 1fr);
+    }
+    .m-cube{
+        display: flex;
+        background-color: white;
+        margin: 10px;
+    }
+</style>
