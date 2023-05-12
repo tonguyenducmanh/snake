86009c804a9c22d3deb24d12d3c649909e58b779
to nguyen duc manh <ducmanh14032000@gmail.com>
2023-05-12 07:51:02 +0700

    them nut setting

--- src/components/MBoard.vue
+++ src/components/MBoard.vue
@@ -1,31 +1,37 @@
 /** màn hình thông tin của trò chơi created by tdmanh1 06/05/2023 */
 <template>
   <div class="m-board">
-    <div class="m-score">Scores: {{ score }}</div>
-    <div class="m-size">
-      <div class="m-size-tile">Game-size: {{ gameSize }} x {{ gameSize }}</div>
-      <div class="m-size-button" @click="gameSizeChange(true)">Up</div>
-      <div class="m-size-button" @click="gameSizeChange(false)">Down</div>
+    <div @click="turnOnSetting" class="m-setting">&#9881;</div>
+    <div v-if="isSetting">
+      <div class="m-size">
+        <div class="m-size-tile">Game-size: {{ gameSize }} x {{ gameSize }}</div>
+        <div class="m-size-button" @click="gameSizeChange(true)">Up</div>
+        <div class="m-size-button" @click="gameSizeChange(false)">Down</div>
+      </div>
+      <div class="m-size">
+        <div class="m-size-tile">Speed</div>
+        <div class="m-size-button" @click="gameSpeedChange(false)">Up</div>
+        <div class="m-size-button" @click="gameSpeedChange(true)">Down</div>
+      </div>
     </div>
-    <div class="m-size">
-      <div class="m-size-tile">Speed</div>
-      <div class="m-size-button" @click="gameSpeedChange(false)">Up</div>
-      <div class="m-size-button" @click="gameSpeedChange(true)">Down</div>
-    </div>
-    <div class="m-move-content">
-      <div class="m-move-item"></div>
-      <div class="m-move-item m-move-button" @click="changePosition(gameConfig.position.up)">
-        &#8593;
-      </div>
-      <div class="m-move-item"></div>
-      <div class="m-move-item m-move-button" @click="changePosition(gameConfig.position.left)">
-        &#8592;
-      </div>
-      <div class="m-move-item m-move-button" @click="changePosition(gameConfig.position.down)">
-        &#8595;
-      </div>
-      <div class="m-move-item m-move-button" @click="changePosition(gameConfig.position.right)">
-        &#8594;
+    <div v-else>
+      <div class="m-score">Scores: {{ score }}</div>
+
+      <div class="m-move-content">
+        <div class="m-move-item"></div>
+        <div class="m-move-item m-move-button" @click="changePosition(gameConfig.position.up)">
+          &#8593;
+        </div>
+        <div class="m-move-item"></div>
+        <div class="m-move-item m-move-button" @click="changePosition(gameConfig.position.left)">
+          &#8592;
+        </div>
+        <div class="m-move-item m-move-button" @click="changePosition(gameConfig.position.down)">
+          &#8595;
+        </div>
+        <div class="m-move-item m-move-button" @click="changePosition(gameConfig.position.right)">
+          &#8594;
+        </div>
       </div>
     </div>
   </div>
@@ -37,7 +43,8 @@ export default {
     return {
       score: 0, // điểm hiện tại của trò chơi
       gameSize: gameConfig.gameSize.defaultSize, // kích cỡ của trò chơi hiện tại
-      gameConfig: gameConfig
+      gameConfig: gameConfig,
+      isSetting: false // bien kiem soat viec co phai dang bat man hinh setting cua game khong
     }
   },
   methods: {
@@ -78,6 +85,14 @@ export default {
       let me = this
       // gọi sự kiện thay đổi vị trí
       me.$emit('changePosition', value)
+    },
+    /**
+     * thay đổi vị trí của con rắn trên màn hình
+     * @author tdmanh1 09-05-2023
+     */
+    turnOnSetting() {
+      let me = this
+      me.isSetting = !me.isSetting
     }
   }
 }
@@ -86,6 +101,7 @@ export default {
 .m-board {
   background-color: black;
   width: 100%;
+  height: 50%;
   border-radius: var(--border-radius);
   display: flex;
   flex-direction: column;
@@ -93,13 +109,14 @@ export default {
   justify-content: center;
   row-gap: var(--padding-common);
   color: white;
-  flex: 1;
+  position: relative;
 }
 .m-score {
   /* background-color: azure; */
   font-size: 1.5rem;
   padding: 10px;
   font-weight: 600;
+  text-align: center;
 }
 .m-move-content {
   /* background-color: crimson; */
@@ -120,9 +137,13 @@ export default {
 .m-size {
   display: flex;
   column-gap: var(--padding-common);
+  font-size: 1.2rem;
+  justify-content: center;
 }
 .m-size-button {
   cursor: pointer;
+  border-radius: var(--border-radius);
+  padding: 0 10px;
 }
 .m-size-button:hover {
   background-color: rgba(139, 139, 139, 0.681);
@@ -136,5 +157,16 @@ export default {
 }
 .m-move-button:active {
   background-color: rgba(120, 120, 120, 0.681);
+}
+.m-setting {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  font-size: 3rem;
+  cursor: pointer;
+}
+.m-setting:hover {
+  background-color: rgba(95, 158, 160, 0.205);
+  border-radius: var(--border-radius);
 }
 </style>
